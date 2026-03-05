f523cb27c4c2e97f609c708e5c27e0acad08802c
FS-ALONE <fs.alone07@gmail.com>
2026-03-05 14:11:44 +0530

    UC1: Add welcome message module

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,12 +1,22 @@
-public class PalindromeCheckerApp {
+class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        //UC1
-         // ===== UC1: Application Entry =====
-         System.out.println("====================================");
-         System.out.println("      WELCOME TO PALINDROME CHECKER");
-         System.out.println("====================================");
-         System.out.println("Application Name : Palindrome Checker App");
-      
+
+        // Welcome Message
+        System.out.println("==================================");
+        System.out.println("      PALINDROME CHECKER APP      ");
+        System.out.println("==================================");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Developed Using  : Java");
+
+        System.out.println("----------------------------------");
+        System.out.println("Welcome! This program checks whether");
+        System.out.println("a given string is a palindrome.");
+        System.out.println("----------------------------------");
+
+        // Program will continue to next use case
     }
 }
